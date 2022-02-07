d48f2b0806b5c151a5c53c1b506ad17a5de9ac0f
chengkangzai <pycck@hotmail.com>
2022-02-07 08:58:16 +0800

    create Q5

---
+++ Tutorial 5/src/com/company/Q5.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Q5 {
+    public static void main(String[] args) {
+    }
+
+    //    f(20, 15) = 20 + 15
+    public static int f(int a, int b) {
+        return a + b;
+    }
+
+}
